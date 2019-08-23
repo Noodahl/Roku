c2d2dca9fd3772d5d4477167fe6eae46a3db531e
Noodahl <36415788+Noodahl@users.noreply.github.com>
2019-08-23 11:29:04 -0400

    Initial Release
    Initiali Release of Roku Module

---
+++ RokuIP/RokuIP/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("RokuIP")]
+[assembly: AssemblyCompany("Crestron Electronics, Inc.")]
+[assembly: AssemblyProduct("RokuIP")]
+[assembly: AssemblyCopyright("Copyright © Crestron Electronics, Inc. 2019")]
+[assembly: AssemblyVersion("1.0.0.*")]
+
---
+++ RokuIP/RokuIP/Roku.cs
@@ -0,0 +1,331 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Crestron.SimplSharp;
+using Crestron.SimplSharp.CrestronIO;
+using Crestron.SimplSharp.Net.Http;
+using Crestron.SimplSharp.CrestronXml;
+using Newtonsoft.Json;
+// For Basic SIMPL# Classes
+
+namespace RokuIP
+{
+    public class Roku
+    {
+        List<sAppInfo> appList;
+        int listSize;
+
+        string baseRequest = "";
+        string requestBody = "";
+        string description = "";
+        static int numRokus;
+        
+        #region Roku IP Connection
+        HttpClient rokuHttpClient;
+        string hostAddress;
+        int portNumber;
+        #endregion
+
+        #region Roku XML Parser
+        XmlReader rokuXMLReader;
+        #endregion
+
+        /// <summary>
+        /// SIMPL+ can only execute the default constructor. If you have variables that require initialization, please
+        /// use an Initialize method
+        /// </summary>
+        public Roku(string Host, int Port, string Description)
+        {
+            try
+            {
+                // IP Address, Port Number -- Username && Password??
+                appList = new List<sAppInfo>(); //Instantiate App List
+
+                if (Port == 0)
+                {
+                    Port = 8060;
+                }
+
+                baseRequest = string.Format(@"http://{0}:{1}/", Host, Port);
+                description = Description;
+                rokuHttpClient = new HttpClient();
+                rokuHttpClient.HostAddress = Host;
+                rokuHttpClient.Port = Port;                
+                //TODO: Define the necessary information for this Roku device
+                numRokus++;
+            }
+            catch (Exception e)
+            {
+                ErrorLog.Error("Error Instantiating Roku, Reason: {0}", e.Message);
+            }
+        }
+
+        #region Prepare Get Request 
+        //Prepare the body of the request for the Roku
+        public void MakeRequest(eRokuRequest RequestToMake)
+        {
+            //TODO: Make a request to the Roku
+            requestBody = "";
+            try
+            {
+                switch (RequestToMake)
+                {
+                    case eRokuRequest.GetAppList:
+                        {
+                            requestBody = "query/apps";
+                            break;
+                        }
+                    case eRokuRequest.GetActiveApp:
+                        {
+                            requestBody = "query/active-app";
+                            break;
+                        }
+                    case eRokuRequest.GetIconByApp:
+                        requestBody = "query/icon/";
+                        break;
+                    case eRokuRequest.KeyPress_Down:
+                        requestBody = "keydown/key";
+                        break;
+                    case eRokuRequest.KeyPress_Up:
+                        requestBody = "keyup/key";
+                        break;
+                    case eRokuRequest.KeyPress_Key:
+                        requestBody = "keypress/";
+                        break;
+                    case eRokuRequest.Launch_App:
+                        requestBody = "launch/";
+                        break;
+                    case eRokuRequest.Install_App:
+                        requestBody = "install/";
+                        break;
+                    default:
+                        break;
+                }
+
+                if (requestBody != "")
+                {
+                    SendRequest(baseRequest + requestBody, RequestToMake);
+                }
+            }
+            catch (Exception e)
+            {
+                ErrorLog.Notice("Error Sending Request, Reason: {0}", e.Message);
+            }
+
+        }
+
+        //Send Remote Command
+        public void MakeRequest(eRokuRequest RequestToMake, eRokuKeyCommand KeyCommand)
+        {
+            //TODO: Make a request to the Roku
+            requestBody = "";
+            try
+            {
+                switch (RequestToMake)
+                {
+                    case eRokuRequest.KeyPress_Key:
+                        requestBody = "keypress/" + KeyCommand.ToString();
+                        ErrorLog.Notice("Command Set to: {0}", requestBody);
+                        break;
+                    default:
+                        break;
+                }
+
+                if (requestBody != "")
+                {
+                    SendRequest(baseRequest + requestBody, RequestToMake);
+                }
+            }
+            catch (Exception e)
+            {
+                ErrorLog.Notice("Error Sending Request, Reason: {0}", e.Message);
+            }
+
+        }
+        #endregion
+
+        #region Send To Roku
+        //Send Command to Roku
+        private void SendRequest(string Request, eRokuRequest RequestMade)
+        {
+            //TODO: Connect To Roku
+            string response = "";
+            try
+            {
+                response = rokuHttpClient.Get(Request);
+                if (response.Length != 0)
+                {
+                    ProcessResponse(response, RequestMade);
+                }                
+            }
+            catch (Exception e)
+            {
+                ErrorLog.Notice("Error Sending Request {0} to Roku", e.Message);
+            }
+        }
+        #endregion
+
+        #region Process Response
+        //Process Response from Roku
+        private void ProcessResponse(string Response, eRokuRequest RequestMade)
+        {
+            rokuXMLReader = new XmlReader(Response);
+            bool getAppIcons = false;
+            switch (RequestMade)
+            {
+                case eRokuRequest.GetAppList:
+                    {
+                        while (rokuXMLReader.Read())
+                        {
+                            switch (rokuXMLReader.NodeType)
+                            {
+                                case XmlNodeType.Element:
+                                    if (rokuXMLReader.HasAttributes)
+                                    {
+                                        while (rokuXMLReader.MoveToNextAttribute())
+                                        {
+                                            if (rokuXMLReader.Name.ToLower() == "app id")
+                                            {
+                                                sAppInfo appInfo = new sAppInfo();
+                                                appInfo.appId = rokuXMLReader.Value;
+                                                appInfo.appName = rokuXMLReader.ReadString();
+                                                appList.Add(appInfo);
+                                                ErrorLog.Notice("App: {0} with ID: {1} added to List", appInfo.appName, appInfo.appId);
+                                                //Get Icon after the List has been generated
+                                            }
+                                        }
+                                    }
+                                    break;
+                                default:
+                                    break;
+                            }
+                        }
+                        if (listSize != appList.Count)
+                        {
+                            getAppIcons = true;
+                            listSize = appList.Count;
+                        }
+                        break;
+                    }
+                case eRokuRequest.GetActiveApp:
+                    while (rokuXMLReader.Read())
+                    {
+                        switch (rokuXMLReader.NodeType)
+                        {
+                            case XmlNodeType.Element:
+                                if (rokuXMLReader.HasAttributes)
+                                {
+                                    while (rokuXMLReader.MoveToNextAttribute())
+                                    {
+                                        if (rokuXMLReader.Name.ToLower() == "app id")
+                                        {
+                                            sAppInfo appInfo = new sAppInfo();
+                                            appInfo.appId = rokuXMLReader.Value;
+                                            appInfo.appName = rokuXMLReader.ReadString();
+                                            appList.Add(appInfo);
+                                            ErrorLog.Notice("App: {0} with ID: {1} added to List", appInfo.appName, appInfo.appId);
+                                            //Get Icon after the List has been generated
+                                        }
+                                    }
+                                }
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                    break;
+                case eRokuRequest.GetIconByApp:
+                    break;
+                case eRokuRequest.KeyPress_Down:
+                    break;
+                case eRokuRequest.KeyPress_Up:
+                    break;
+                case eRokuRequest.KeyPress_Key:
+                    break;
+                case eRokuRequest.Launch_App:
+                    break;
+                case eRokuRequest.Install_App:
+                    break;
+                default:
+                    break;
+            }
+            if (getAppIcons)
+            {
+                //TODO: Request App Icon List
+            }
+        }
+
+        private void UpdateIcons()
+        {
+            //TODO: Move to a Thread
+
+            foreach (var app in appList)
+            {
+                rokuHttpClient.Get(baseRequest + "query/apps/icon/" + app.appId);
+            }
+        }
+        #endregion
+
+        #region Enumerations
+
+        public enum eRokuRequest
+        {
+            //TODO: List of Requests which can be made to the Roku Device
+            GetAppList,
+            GetActiveApp,
+            GetIconByApp,
+            KeyPress_Down,
+            KeyPress_Up,
+            KeyPress_Key,
+            Launch_App,
+            Install_App
+        }
+
+        public enum eRokuResponseType
+        {
+            //TODO: List of all responses that the Roku may return
+        }
+
+        public enum eRokuKeyCommand
+        {
+              Home,
+              Rev,
+              Fwd,
+              Play,
+              Select,
+              Left,
+              Right,
+              Down,
+              Up,
+              Back,
+              InstantReplay,
+              Info,
+              Backspace,
+              Search,
+              Enter
+        }
+
+        #endregion
+
+        #region Structore
+
+        private struct sAppInfo
+        {
+            public string appName { get; set; }
+            public string appIcon { get; set; }
+            public string appId { get; set; }
+        }
+
+        #endregion
+
+        #region Event
+        public event EventHandler<RokuEventArgs> onResponseProcessed;
+        #endregion
+    }
+
+    public class RokuEventArgs : EventArgs
+    {
+        //TODO: Event Arguments for Roku
+    }
+
+}
